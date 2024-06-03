af09c05dd6ae79d2edb7fbfea6798bced6a65bb0
Mister Frize <android19931@gmail.com>
2024-06-03 15:48:36 +0300

    Project Task #2

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+class User:
+    def __init__(self, name=None, second_name=None):
+        self.name = name
+        self.second_name = second_name
+
+    def create(self):
+        self.name = "Dmytro"
+        self.second_name = "Maltsev"
+
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+    yield user
+    user.remove()
